2da5efbbc782edafd5d8238979e7fdc53289baf1
F1sher <dasalam@gmail.com>
2018-01-15 22:54:12 +0300

    Added send_comm

---
+++ send_comm.c
@@ -0,0 +1,28 @@
+#include "dsp.h"
+#include "parse_args.h"
+
+int main(int argc, char **argv)
+{
+    int res = 0;
+    unsigned int time = 100;
+    cyusb_handle *usb_h = NULL;
+
+    res = init_controller(&usb_h);
+	if (res != 0) {
+		fprintf(stderr, "Error in init_controller()\n");
+
+        _exit(-1);
+	}
+
+    printf("argv = %s\n", *argv);
+    const char *out_foldername = parse_and_give_comm(argc, argv, usb_h, &time);
+    if (out_foldername == NULL) {
+        exit_controller(usb_h);
+        
+        _exit(-2);
+    }
+
+    exit_controller(usb_h);
+
+    _exit(0);
+}
